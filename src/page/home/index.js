2b57d638e290d4d9eb19276805714db3ae784800
吴广诺 <wuguangnuo@qq.com>
2019-01-13 16:32:33 +0800

    import antd and make a demo

--- src/page/home/index.js
+++ src/page/home/index.js
@@ -1,44 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 // import ReactDOM from 'react-dom';
 
-// import { Layout, Row, Col, Avatar, Input, Menu, Dropdown, Icon, Badge  } from 'antd';
-// import 'antd/dist/antd.css';  // or 'antd/dist/antd.less'
-
-// import SiderComponent from './sider'
-// import Tooltip from '../component/tooltip'
-// import MyNews from '../component/myNews'
-
-// const { Header, Footer, Sider, Content  } = Layout;
-
-// 
-// const menu = (
-//     <div>
-//         const menu
-//     </div>
-// );
+import {
+    version, Button
+    , LocaleProvider, DatePicker, message
+} from 'antd';
+import "antd/dist/antd.css";
+// 由于 antd 组件的默认文案是英文，所以需要修改为中文
+import zhCN from 'antd/lib/locale-provider/zh_CN';
 
 export default class Index extends Component {
-    constructor (props) {
-        super(props)
+    constructor(props) {
+        super(props);
         this.state = {
-            value: '1',
-            valueTxt: '',
-        }
+            date: ''
+        };
     }
 
-    // getValue (val,value) {
-    //     this.setState ({
-    //         value: val,
-    //         valueTxt: value
-    //     })
-    // }
+    handleChange(date) {
+        message.info('您选择的日期是: ' + (date ? date.toString() : ''));
+        this.setState({ date });
+    }
 
-    render () {
+    render() {
         return (
-            <div>
-                index!
-                {this.props.children}
-            </div> 
+            <div className="App">
+                <p>Current antd version: {version}</p>
+                <Button type="primary">Hello</Button>
+                <LocaleProvider locale={zhCN}>
+                    <div style={{ width: 400, margin: '100px auto' }}>
+                        <DatePicker onChange={value => this.handleChange(value)} />
+                        <div style={{ marginTop: 20 }}>当前日期：{this.state.date && this.state.date.toString()}</div>
+                    </div>
+                </LocaleProvider>
+            </div>
         )
     }
 }
--- src/routes/routerPage.js
+++ src/routes/routerPage.js
@@ -1,5 +1,5 @@
 import React from 'react'  
-import { Route, BrowserRouter as Router, Link, Switch, Redirect } from 'react-router-dom'  
+import { Route, BrowserRouter as Router, /* Link, */ Switch, Redirect } from 'react-router-dom'  
 
 // 引入所有基础配置
 //import '../config/globalConfig'
